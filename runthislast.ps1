89f1f69c2a4b95cf73de739d25c436128f341f04
saabismi <58708865+saabismi@users.noreply.github.com>
2020-01-29 13:12:24 +0200

    Delete runthislast.ps1

--- runthislast.ps1
+++
@@ -1,7 +0,0 @@
-#Script for setting the ExecutionPolicy value to the default value so that the computer 
-#is not left vulnerable to unwanted Powershell scripts.
-
-Write-Output "Set ExecutionPolicy value to Default."
-Set-ExecutionPolicy Default
-
-Pause
