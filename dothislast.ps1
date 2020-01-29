b38600fccb08396e652fca19760faedd5ce5b1cb
saabismi <58708865+saabismi@users.noreply.github.com>
2020-01-29 10:52:59 +0200

    Delete dothislast.ps1

--- dothislast.ps1
+++
@@ -1,7 +0,0 @@
-#Script for setting the Set-ExecutionPolicy value to the default value so that the computer 
-#is not left vulnerable to unwanted Powershell scripts.
-
-Write-Output "Set Set-ExecutionPolicy value to Default."
-Set-ExecutionPolicy Default
-
-Pause
